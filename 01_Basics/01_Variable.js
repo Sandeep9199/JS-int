3bd70ae82fe1efef883fd050379a525b69d5a1ff
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-02-13 18:22:05 +0530

    scene of variables

--- 01_Basics/01_Variable.js
+++ 01_Basics/01_Variable.js
@@ -1,1 +1,20 @@
 
+const accountId = 144553
+// accountId = 2 // not allowed because of declaration of const
+let accountEmail = "sandeep@google.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+accountEmail = "sandeep@sharma.com"
+accountPassword = "62045344544"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
